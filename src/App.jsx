30226f44b2701475e4de601b9a2160035cf2d31b
Exohspoer <kakashihatake6hokagegygg@gmail.com>
2025-10-31 23:56:49 +0200

    create info section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-export default function App() {
+import React from "react";
+import {Routes, Route, NavLink} from "react-router-dom"
+import Home from "./page/Home";
+
+const App = () => {
+  
+  
   return (
-    <>
-      <p>ezzz</p>
-    </>
-  );
+    <Routes>
+      <Route path='/' element={<Home />} />
+    </Routes>
+  )
 }
+
+export default App
---
+++ src/page/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Info from '../components/Info';
+const Home = () => {
+  return (
+    <div>
+        <Info />
+    </div>
+  )
+}
+
+export default Home;

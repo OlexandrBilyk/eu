97922d8951034c3ff670d1a05301464e4bdfe41e
OlexandrBilyk <0500423510s@gmail.com>
2025-11-01 15:21:53 +0200

    change header sizes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,34 @@
 import styles from "./Header.module.scss";
+import { Link } from "react-router-dom";
+import { FaSearch } from "react-icons/fa";
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <div className={styles.logoSection}>
-        <img src="https://flagcdn.com/eu.svg" alt="EU" className={styles.flag} />
-        <div className={styles.title}>
-          <strong>Європейський</strong> союз
-        </div>
-      </div>
+      <div className={`container ${styles.container}`}>
+        <Link to='/' className={styles.logoSection}>
+          <img
+            src="https://flagcdn.com/eu.svg"
+            alt="EU"
+            className={styles.flag}
+          />
+          <div className={styles.title}>
+            <strong>Європейський</strong> союз
+          </div>
+        </Link>
 
-      <div className={styles.right}>
-        <select className={styles.lang}>
-          <option>русский</option>
-          <option>українська</option>
-          <option>English</option>
-        </select>
-        <div className={styles.search}>
-          <input type="text" placeholder="Поиск" />
-          <button></button>
+        <div className={styles.right}>
+          <select className={styles.lang}>
+            <option>українська</option>
+            <option>русский</option>
+            <option>English</option>
+          </select>
+          <div className={styles.search}>
+            <input type="text" />
+            <button className={styles.btnSearch}>
+              <FaSearch></FaSearch>Пошук
+            </button>
+          </div>
         </div>
       </div>
     </header>
